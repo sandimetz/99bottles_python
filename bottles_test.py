6ea946460862c8c00876c2d952797aeb7d736823
Katrina Owen <katrina.owen@gmail.com>
2023-11-13 21:40:16 +0100

    verse 99 test

--- bottles_test.py
+++ bottles_test.py
@@ -3,4 +3,11 @@ from bottles import Bottles
 
 
 class BottlesTest(unittest.TestCase):
-  pass
+  def test_the_first_verse(self):
+    expected = (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+    )
+    self.assertEqual(Bottles().verse(99), expected)
