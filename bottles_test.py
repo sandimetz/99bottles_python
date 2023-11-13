298fe40f5ec02f9aa06224094bb391ea6bc416fa
Katrina Owen <katrina.owen@gmail.com>
2023-11-13 21:40:16 +0100

    verse 99 test FAILS

--- bottles_test.py
+++ bottles_test.py
@@ -1,3 +1,11 @@
 from bottles import Bottles
 
 
+def test_the_first_verse():
+  expected = (
+    '99 bottles of beer on the wall, '
+    '99 bottles of beer.\n'
+    'Take one down and pass it around, '
+    '98 bottles of beer on the wall.\n'
+  )
+  assert Bottles().verse(99) == expected
