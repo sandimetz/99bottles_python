2be985f467daaeef6e6836b5729e59a45a206f89
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 11:19:59 +0100

    pass 1 test

--- bottles.py
+++ bottles.py
@@ -1,15 +1,24 @@
 class Bottles:
   def verse(self, number):
-    if (number == 2):
-      return (
-        '2 bottles of beer on the wall, '
-        '2 bottles of beer.\n'
-        'Take one down and pass it around, '
-        '1 bottle of beer on the wall.\n'
-      )
-    return (
-      f'{number} bottles of beer on the wall, '
-      f'{number} bottles of beer.\n'
-      f'Take one down and pass it around, '
-      f'{number-1} bottles of beer on the wall.\n'
-    )
+    match number:
+      case 1:
+        return (
+          '1 bottle of beer on the wall, '
+          '1 bottle of beer.\n'
+          'Take it down and pass it around, '
+          'no more bottles of beer on the wall.\n'
+        )
+      case 2:
+        return (
+          '2 bottles of beer on the wall, '
+          '2 bottles of beer.\n'
+          'Take one down and pass it around, '
+          '1 bottle of beer on the wall.\n'
+        )
+      case _:
+        return (
+          f'{number} bottles of beer on the wall, '
+          f'{number} bottles of beer.\n'
+          f'Take one down and pass it around, '
+          f'{number-1} bottles of beer on the wall.\n'
+        )
