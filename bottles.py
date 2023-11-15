c6a7f44e51bbbd1be7bbe0c81745614ba37ad6f3
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 10:58:23 +0100

    pass 2 test with outer conditional

--- bottles.py
+++ bottles.py
@@ -1,5 +1,12 @@
 class Bottles:
   def verse(self, number):
+    if (number == 2):
+      return (
+        '2 bottles of beer on the wall, '
+        '2 bottles of beer.\n'
+        'Take one down and pass it around, '
+        '1 bottle of beer on the wall.\n'
+      )
     return (
       f'{number} bottles of beer on the wall, '
       f'{number} bottles of beer.\n'
