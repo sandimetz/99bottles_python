a33d6869d8114b7bab1cf978a36d211b078c5174
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 10:38:44 +0100

    pass 3 and 99 with conditional

--- bottles.py
+++ bottles.py
@@ -1,8 +1,15 @@
 class Bottles:
-  def verse(self, _):
+  def verse(self, number):
+    if (number == 99):
+      return (
+        '99 bottles of beer on the wall, '
+        '99 bottles of beer.\n'
+        'Take one down and pass it around, '
+        '98 bottles of beer on the wall.\n'
+      )
     return (
-      '99 bottles of beer on the wall, '
-      '99 bottles of beer.\n'
+      '3 bottles of beer on the wall, '
+      '3 bottles of beer.\n'
       'Take one down and pass it around, '
-      '98 bottles of beer on the wall.\n'
+      '2 bottles of beer on the wall.\n'
     )
