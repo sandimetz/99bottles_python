120400c07eef90fb1c8528107f9e9e3bfed1216f
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 11:40:00 +0100

    verse 0 test FAILS

--- bottles_test.py
+++ bottles_test.py
@@ -39,3 +39,13 @@ def test_verse_1():
     'no more bottles of beer on the wall.\n'
   )
   assert Bottles().verse(1) == expected
+
+
+def test_verse_0():
+  expected = (
+    'No more bottles of beer on the wall, '
+    'no more bottles of beer.\n'
+    'Go to the store and buy some more, '
+    '99 bottles of beer on the wall.\n'
+  )
+  assert Bottles().verse(0) == expected
