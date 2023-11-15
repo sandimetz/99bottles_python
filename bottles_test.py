4b525e3499b08687cf3277f81bc2c634c22987bd
Katrina Owen <katrina.owen@gmail.com>
2023-11-15 11:44:28 +0100

    verses test #1

--- bottles_test.py
+++ bottles_test.py
@@ -47,3 +47,17 @@ class BottlesTest(unittest.TestCase):
       '99 bottles of beer on the wall.\n'
     )
     self.assertEqual(Bottles().verse(0), expected)
+
+  def test_a_couple_verses(self):
+    expected = (
+      '99 bottles of beer on the wall, '
+      '99 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '98 bottles of beer on the wall.\n'
+      '\n'
+      '98 bottles of beer on the wall, '
+      '98 bottles of beer.\n'
+      'Take one down and pass it around, '
+      '97 bottles of beer on the wall.\n'
+    )
+    self.assertEqual(Bottles().verses(99, 98), expected)
