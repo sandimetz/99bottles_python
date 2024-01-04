05189a8316cb7b80dd5d11696ccd34a9764ac915
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 12:14:57 +0100

    make case 0 f-strings

--- bottles.py
+++ bottles.py
@@ -9,10 +9,10 @@ class Bottles:
     match number:
       case 0:
         return (
-          'No more bottles of beer on the wall, '
-          'no more bottles of beer.\n'
-          'Go to the store and buy some more, '
-          '99 bottles of beer on the wall.\n'
+          f'No more bottles of beer on the wall, '
+          f'no more bottles of beer.\n'
+          f'Go to the store and buy some more, '
+          f'99 bottles of beer on the wall.\n'
         )
       case _:
         return (
