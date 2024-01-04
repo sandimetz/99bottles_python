43ec37fda7cd0bf5c79a69d78396a9ad53c3c76b
Katrina Owen <katrina.owen@gmail.com>
2024-01-04 11:10:45 +0100

    make 1 case f-strings

--- bottles.py
+++ bottles.py
@@ -16,10 +16,10 @@ class Bottles:
         )
       case 1:
         return (
-          '1 bottle of beer on the wall, '
-          '1 bottle of beer.\n'
-          'Take it down and pass it around, '
-          'no more bottles of beer on the wall.\n'
+          f'1 bottle of beer on the wall, '
+          f'1 bottle of beer.\n'
+          f'Take it down and pass it around, '
+          f'no more bottles of beer on the wall.\n'
         )
       case _:
         return (
