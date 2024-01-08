3508e3ab8231d6c404f7f4ada733b80e0fcc36e5
Katrina Owen <katrina.owen@gmail.com>
2024-01-08 12:17:50 +0100

    reduce action to default in superclass

--- bottles.py
+++ bottles.py
@@ -40,8 +40,6 @@ class BottleNumber:
     return 'bottles'
 
   def action(self):
-    if self.number == 0:
-        return 'Go to the store and buy some more'
     return f'Take {self.pronoun()} down and pass it around'
 
   def pronoun(self):
