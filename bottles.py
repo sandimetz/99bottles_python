f5155d7e9179f25b3d2447db75ad414c28bd4adb
Katrina Owen <katrina.owen@gmail.com>
2024-01-09 12:05:05 +0100

    use successor everywhere

--- bottles.py
+++ bottles.py
@@ -13,7 +13,8 @@ class Bottles:
           ' of beer on the wall, '
           f'{self.quantity(number)} {self.container(number)} of beer.\n'
           f'{self.action(number)}, '
-          f'{self.quantity(self.successor(number))} {self.container(number-1)}'
+          f'{self.quantity(self.successor(number))} '
+          f'{self.container(self.successor(number))}'
           ' of beer on the wall.\n'
         )
       case _:
@@ -22,7 +23,8 @@ class Bottles:
           ' of beer on the wall, '
           f'{self.quantity(number)} {self.container(number)} of beer.\n'
           f'{self.action(number)}, '
-          f'{self.quantity(self.successor(number))} {self.container(number-1)}'
+          f'{self.quantity(self.successor(number))} '
+          f'{self.container(self.successor(number))}'
           ' of beer on the wall.\n'
         )
 
